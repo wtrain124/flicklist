d5fcb6e5f8ce44293a0e2a5a079067d889bf9345
Will Train <wtrain124@gmail.com>
2019-01-14 19:07:24 -0600

    first

--- js/flicklist.js
+++ js/flicklist.js
@@ -8,7 +8,7 @@ var model = {
 
 var api = {
   root: "https://api.themoviedb.org/3",
-  token: "TODO" // TODO 0 put your api key here
+  token: "65b073e78911ae5c8f4fdd144d9d960f" // TODO 0 put your api key here
 }
 
 
@@ -29,7 +29,7 @@ function discoverMovies(callback) {
 			
 			// TODO 2
 			// update the model, setting its .browseItems property equal to the movies we recieved in the response
-			
+			model.browseItems = response.results
 			// invoke the callback function that was passed in. 
 			callback();
 		}
@@ -44,22 +44,47 @@ function discoverMovies(callback) {
 function render() {
   // TODO 7
   // clear everything from both lists
+	$('section-watchlist ul').empty()
+	$('section-browse ul').empty()
   
   // TODO 6
   // for each movie on the user's watchlist, insert a list item into the <ul> in the watchlist section
-  
+  model.watchlistItems.forEach(function(movie) {
+		
+		let listItem = $('<li></li>').text(movie.title)
+
+    //let button = $('<input type="button" value="Add to Watchlist" />')
+    //button.click((event)=>addToWatchlist(movie)) // TODO 5
+		//listItem.append(button)  // TODO 4
+		
+		$('section-watchlist ul').append(listItem) // TODO 3
+		
+	
+  });
+
+
+
   // for each movie on the current browse list, 
   model.browseItems.forEach(function(movie) {
-		// TODO 3
-		// insert a list item into the <ul> in the browse section
 		
-		// TODO 4
-		// the list item should include a button that says "Add to Watchlist"
+		let listItem = $('<li></li>').text(movie.title)
+
+    let button = $('<input type="button" value="Add to Watchlist" />')
+    button.click((event)=>addToWatchlist(movie)) // TODO 5
+		listItem.append(button)  // TODO 4
 		
-		// TODO 5
-		// when the button is clicked, this movie should be added to the model's watchlist and render() should be called again
+		$('section-browse ul').append(listItem) // TODO 3
+		
+	
   });
   
+
+function addToWatchlist(movie){
+	   model.watchlistItems.push(movie)
+		 render()
+}
+
+
 }
 
 
